8616ffe6a2c4b0a57d7f84df431f4ffbbe48ffb4
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-03-05 13:34:22 -0500

    updated orm.js file

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,22 @@
 const orm = require(`../config/orm`)
+
+const burger = {
+    all(cb) {
+      orm.all('burgers', (res) => cb(res));
+    },
+
+    create(cols, vals, cb) {
+      orm.create('burgers', cols, vals, (res) => cb(res));
+    },
+    update(objColVals, condition, cb) {
+      orm.update('burgers', objColVals, condition, (res) => cb(res));
+    },
+    delete(condition, cb) {
+      orm.delete('burgers', condition, (res) => cb(res));
+    },
+  };
+  
+  
+  create 
+
+  module.exports = burger;
