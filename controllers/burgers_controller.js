e0317200b8b8462ad66e214d3930acd0c1fe710a
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-03-05 00:38:29 -0500

    setting up exports between files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,15 @@ const express = require(`express`);
 
 const router = express.Router();
 
+const burger = require(`../models/burger`);
+
+router.get(`/`, (req, res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            
+        }
+    })
+})
 
 
 
