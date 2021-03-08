45f85e229f8aa2216b1c917edadef534eb20c0ae
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-03-07 22:11:26 -0500

    testing heroku, go back one step if error populates

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,13 @@
 //CONTAINS CODE FOR LINKING JS TO MYSQL WORKBENCH
 //////////////////////////////////////////////////////////////////
 const mysql = require(`mysql`);
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+var connection;
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
 // port used in mysql for burgers_db
 port: 3306,
 
@@ -13,16 +18,19 @@ user: `root`,
 //password specific to this class for safety of data.
 password: `yourRootPassword`,
 database: `burgers_db`,
-});
+})
+};
+
 
 //will display error when connecting, if any.
-connection.connect((err) => {
-    if (err) {
-      console.error(`error connecting: ${err.stack}`);
-      return;
-    }
-    console.log(`connected as id ${connection.threadId}`);
-  });
+// connection.connect((err) => {
+//     if (err) {
+//       console.error(`error connecting: ${err.stack}`);
+//       return;
+//     }
+//     console.log(`connected as id ${connection.threadId}`);
+  // });
 
 //module.exports here.
+connection.connect();
 module.exports = connection;
