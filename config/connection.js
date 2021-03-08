5eaa791d15de9e77a80b8d4e46becf3fa6d4aac4
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-03-07 22:41:35 -0500

    MAJOR ERROR APP DOWN

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,8 @@
 //CONTAINS CODE FOR LINKING JS TO MYSQL WORKBENCH
 //////////////////////////////////////////////////////////////////
 const mysql = require(`mysql`);
-// const connection = mysql.createConnection({
-var connection;
+const connection = mysql.createConnection({
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-connection = mysql.createConnection({
 // port used in mysql for burgers_db
 port: 3306,
 
@@ -18,19 +13,16 @@ user: `root`,
 //password specific to this class for safety of data.
 password: `yourRootPassword`,
 database: `burgers_db`,
-})
-};
-
+});
 
 //will display error when connecting, if any.
-// connection.connect((err) => {
-//     if (err) {
-//       console.error(`error connecting: ${err.stack}`);
-//       return;
-//     }
-//     console.log(`connected as id ${connection.threadId}`);
-  // });
+connection.connect((err) => {
+    if (err) {
+      console.error(`error connecting: ${err.stack}`);
+      return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+  });
 
 //module.exports here.
-connection.connect();
 module.exports = connection;
